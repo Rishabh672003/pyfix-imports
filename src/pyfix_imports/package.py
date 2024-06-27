614cd1c27ee9f0202c6dddff1b79e6b5a93d6f6f
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-06-27 12:26:32 +0530

    fix: previous way was better

--- src/pyfix_imports/package.py
+++ src/pyfix_imports/package.py
@@ -28,20 +28,23 @@ def is_package(name: str) -> str | None:
 
 def import_string(mod_set: Set[str]) -> str:
     predefined_keys = set(predefined_imports.keys())
+
+    types = []
+    collections = []
     predefined_imports_list = []
     other_imports = []
 
-    types = mod_set & typing_imports()
-    collections = mod_set & collection_imports()
-
     for mod in mod_set:
-        if (mod not in types) and (mod not in collections):
-            if mod in predefined_keys:
-                predefined_imports_list.append(predefined_imports[mod])
-            else:
-                package_import = is_package(mod)
-                if package_import:
-                    other_imports.append(package_import)
+        if mod in typing_imports():
+            types.append(mod)
+        elif mod in collections:
+            collections.append(mod)
+        elif mod in predefined_keys:
+            predefined_imports_list.append(predefined_imports[mod])
+        else:
+            package_import = is_package(mod)
+            if package_import:
+                other_imports.append(package_import)
 
     typings_str = "from typing import " + ", ".join(types) if types else ""
     collections_str = (
