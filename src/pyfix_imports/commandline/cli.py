06b70ca7e9ebdb724e6f5433381a341dbc56641e
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-08-05 21:49:44 +0530

    fix: cli() won't return anything

--- src/pyfix_imports/commandline/cli.py
+++ src/pyfix_imports/commandline/cli.py
@@ -5,7 +5,6 @@ import click
 from pyfix_imports.file import write_to_file
 from pyfix_imports.fix_code import fix_code
 
-
 @click.command()
 @click.option(
     "-f", "--fix", is_flag=True, default=False, help="format the file in place"
@@ -17,12 +16,9 @@ from pyfix_imports.fix_code import fix_code
     type=click.Path(exists=True, dir_okay=False, readable=True),
     help="path of the config file",
 )
-@click.argument(
-    "filename",
-    type=click.Path(exists=True, dir_okay=False, readable=True),
-)
-def cli(filename: str, fix: bool, config_file: Path | None) -> str | None:
-    fixed_code = fix_code(filename, config_file)
+@click.argument("filename", type=click.Path(exists=True, dir_okay=False, readable=True))
+def cli(filename: str, fix: bool, config_file: Path | None) -> None:
+    fixed_code: str = fix_code(filename, config_file)
 
     if not fix:
         click.echo(fixed_code.lstrip(), nl=True)
