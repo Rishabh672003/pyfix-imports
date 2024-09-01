07bf70d208639af14ace79e7f2342f0fad270875
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-09-01 10:14:50 +0530

    refactor: improve type annotations

--- src/pyfix_imports/file.py
+++ src/pyfix_imports/file.py
@@ -12,7 +12,7 @@ def get_file_text(filename: str) -> str:
     return output
 
 
-def write_to_file(filename: str, text: str):
+def write_to_file(filename: str, text: str) -> None:
     try:
         with open(filename, "w") as file:
             file.write(text)
